a1323166c737264ad0b14e0d9539eab4a589f5bb
Notho-freedom <bobymomo6@gmail.com>
2025-08-16 18:46:17 +0300

    Ajout de la dépendance "@heroicons/react" et mise à jour des composants de l'interface utilisateur, incluant des améliorations de style et de nouvelles icônes dans la liste de discussion et la barre latérale.

--- src/components/Titlebar.jsx
+++ src/components/Titlebar.jsx
@@ -8,7 +8,7 @@ export default function Titlebar() {
       style={{ WebkitAppRegion: 'drag' }} // <- zone draggable par défaut
     >
       {/* Zone gauche : draggable */}
-      <div className="flex items-center px-0 py-6 space-x-3">
+      <div className="flex items-center px-0 py-6 space-x-3 absolute z-[100]">
         <FaWhatsapp className="w-6 h-6 text-green-500" />
         <span className="tracking-wide text-white font-segoe">
           WhatsApp
@@ -17,24 +17,24 @@ export default function Titlebar() {
 
       {/* Zone droite : pas draggable */}
       <div
-        className="flex items-center space-x-2 text-[10px] font-semibold"
+        className="flex items-center text-[10px] top-0 absolute right-0"
         style={{ WebkitAppRegion: 'no-drag' }} // <- on exclut les boutons
       >
         <button
           aria-label="Minimize"
-          className="flex items-center justify-center w-4 h-4 transition-colors rounded-sm hover:bg-gray-600"
+          className="flex items-center justify-center w-12 h-8 transition-colors hover:bg-whatsapp-dark-700/80"
         >
           <Minus size={12} />
         </button>
         <button
           aria-label="Maximize"
-          className="flex items-center justify-center w-4 h-4 transition-colors rounded-sm hover:bg-gray-600"
+          className="flex items-center justify-center w-12 h-8 transition-colors hover:bg-whatsapp-dark-700/80"
         >
           <Square size={12} />
         </button>
         <button
           aria-label="Close"
-          className="flex items-center justify-center w-4 h-4 transition-colors rounded-sm hover:bg-red-600"
+          className="flex items-center justify-center w-12 h-8 transition-colors hover:bg-red-600"
         >
           <X size={12} />
         </button>
