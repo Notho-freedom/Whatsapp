ac1f2bf46db2cea01f2c6e709eaa5e13075895ac
Notho-freedom <bobymomo6@gmail.com>
2025-08-31 04:02:26 +0300

    feat: Amélioration de l'indicateur de chargement dans le composant ChatList. Changement de la taille de l'animation de chargement et ajustement de la mise en page pour une meilleure expérience utilisateur lors du chargement des conversations.

--- src/components/chat/ChatList.jsx
+++ src/components/chat/ChatList.jsx
@@ -491,13 +491,12 @@ export default function ChatList({ onChatSelect, selectedChatId, onStatusSelect,
               </div>
             )
           ) : (
-            // Mode Chats
-            filteredUsers.length === 0 ? (
-              <div className="p-4 text-center text-gray-400">
-                <div className="animate-spin rounded-full h-6 w-6 border-b-2 border-[#1DAA61] mx-auto mb-2"></div>
-                <p>Chargement des conversations...</p>
-              </div>
-            ) : sortedUsers.length === 0 ? (
+                         // Mode Chats
+             filteredUsers.length === 0 ? (
+               <div className="flex items-center justify-center h-full">
+                 <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-[#1DAA61] mt-[45%]"></div>
+               </div>
+             ) : sortedUsers.length === 0 ? (
               <div className="p-4 text-center text-gray-400">
                 <p>Aucune conversation trouvée</p>
               </div>
